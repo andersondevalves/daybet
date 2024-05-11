df90fc15b4233edddc9d000449b8fe0aff18f563
Anderson Alves <anderson.asantana05@gmail.com>
2024-05-10 21:24:07 -0300

    esquema de cores

---
+++ lib/src/componets/color_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle{
+  static const primary = Color.fromRGBO(20,200,113, 1.0);
+  static const secondary = Color.fromRGBO(194, 194, 194,1);
+  static const backgroud = Color.fromRGBO(194, 194, 194,1);
+}
