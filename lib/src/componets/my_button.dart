58cb491913af77414dfdfa6f70431cf227f94b4b
Anderson Alves <anderson.asantana05@gmail.com>
2024-05-13 21:03:56 -0300

    cria componente de botao

---
+++ lib/src/componets/my_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'color_style.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+
+  required double buttonPropotion,
+  required double marginSize,
+  required String label,
+  required bool isPrimary,
+  required VoidCallback onPressedButton,
+  }): _buttonPropotion = buttonPropotion,
+      _marginSize = marginSize,
+      _label = label,
+      _isPrimary = isPrimary,
+      _onPressedButton = onPressedButton;
+
+  final double _buttonPropotion;
+  final double _marginSize;
+  final String _label;
+  final bool _isPrimary;
+  final VoidCallback _onPressedButton;
+
+  @override 
+  Widget build(BuildContext context){
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * _buttonPropotion - _marginSize,
+      height: 62,
+      child: OutlinedButton(
+        onPressed: _onPressedButton,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: _isPrimary ? ColorStyle.primary : ColorStyle.secondary,
+          side:  const BorderSide(
+           color: ColorStyle.primary,
+          ),
+          shape: RoundedRectangleBorder(
+        BorderRadius : BorderRadius.circular(10)
+          )
+        ),
+        child: Text(
+          _label,
+          style: TextStyle(
+           color: _isPrimary ? Colors.white : Colors.black
+          )
+        ), 
+      ),
+    ),
+  }
+
+
+
+}
